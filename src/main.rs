63ce1f94aff78d2951b4d25ecef503d924692cbb
youngbloood <944168348@qq.com>
2024-01-16 17:24:17 +0800

    feat: adapt \\w

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,32 @@ use std::io;
 use std::process;
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
+    // match digits
     if pattern.eq("\\d") {
+        if input_line.len() == 0 {
+            return false;
+        }
         let digital = input_line.parse::<i128>();
         if digital.is_err() {
             return false;
         }
         return true;
+    }
+    // match alphanumeric
+    // (a-z, A-Z, 0-9, _)
+    if pattern.eq("\\w") {
+        if input_line.len() == 0 {
+            return false;
+        }
+        let first = *input_line.as_bytes().into_iter().next().unwrap();
+        if (first >= 'a' as u8 && first <= 'z' as u8)
+            || (first >= 'A' as u8 && first <= 'Z' as u8)
+            || (first >= '0' as u8 && first <= '9' as u8)
+            || first == '_' as u8
+        {
+            return true;
+        }
+        return false;
     }
 
     if pattern.chars().count() == 1 {
